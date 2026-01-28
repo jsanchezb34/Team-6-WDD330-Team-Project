968c6f96945e6db9fa85ee147aeed8abc737ddb5
ngahirinuki <hir23003@byui.edu>
2026-01-29 12:38:02 +1300

    Everett js total code

---
+++ src/js/cart.js
@@ -0,0 +1,53 @@
+import { getLocalStorage } from "./utils.mjs";
+
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart");
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+}
+
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
+
+  return newItem;
+}
+
+renderCartContents();
+
+function renderCartContents() {
+  let cartItems = getLocalStorage("so-cart");
+ 
+  if (cartItems && Array.isArray(cartItems)) {
+ 
+    cartItems = cartItems.filter(item => item !== null);
+ 
+    if (cartItems.length > 0) {
+ 
+      document.querySelector(".cart-footer").classList.remove("hide");
+ 
+      const total = cartItems.reduce((sum, item) => sum + (item.FinalPrice || 0), 0);
+      document.querySelector(".cart-total").innerText = `Total: $${total.toFixed(2)}`;
+ 
+      const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+      document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    } else {
+      showEmptyCart();
+    }
+  } else {
+    showEmptyCart();
+  }
+}
+ 
