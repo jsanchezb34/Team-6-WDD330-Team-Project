b663582acd342bc7b876010ca0be76c90c13fd15
ngahirinuki <hir23003@byui.edu>
2026-01-31 04:29:27 +1300

    W03 Individual Activity: Expand Inventory done

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,17 +1,21 @@
-//this file handles the retrieval of product data from a JSON file
+const baseURL = import.meta.env.VITE_SERVER_URL;
 
+// Handles retrieval of product data from API
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../public/json/${this.category}.json`;
+  constructor() {}
+
+  async getData(category) {
+    console.log(`${baseURL}products/search/${category}`);
+
+    const response = await fetch(
+      `${baseURL}products/search/${category}`
+    );
+    const data = await convertToJson(response);
+    return data.Result;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
-  }
-  async findProductById(id) {
-    const products = await this.getData();
+
+  async findProductById(id, category) {
+    const products = await this.getData(category);
     return products.find((item) => item.Id === id);
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
+const category = getParam('category');
+
+const titleElement = document.querySelector('.page-title');
+if (titleElement) {
+  titleElement.textContent = `Top Products: ${category.charAt(0).toUpperCase() + category.slice(1)}`;
+}
+
+const dataSource = new ProductData();
+
 
 const element = document.querySelector(".product-list");
 
-const productList = new ProductList("Tents", dataSource, element);
+const productList = new ProductList(category, dataSource, element);
+
 
 productList.init();
