0dd5ce9c87f7db818929426a895a9ac26133fcc8
ngahirinuki <hir23003@byui.edu>
2026-01-30 21:38:03 +1300

    stopped as incomplete

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,27 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  let cartItems = getLocalStorage("so-cart");
+ 
+  if (cartItems && Array.isArray(cartItems)) {
+ 
+    cartItems = cartItems.filter(item => item !== null);
+ 
+    if (cartItems.length > 0) {
+ 
+      document.querySelector(".cart-footer").classList.remove("hide");
+ 
+      const total = cartItems.reduce((sum, item) => sum + (item.FinalPrice || 0), 0);
+      document.querySelector(".cart-total").innerText = `Total: $${total.toFixed(2)}`;
+ 
+      const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+      document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    } else {
+      showEmptyCart();
+    }
+  } else {
+    showEmptyCart();
+  }
 }
 
 function cartItemTemplate(item) {
@@ -27,27 +45,5 @@ function cartItemTemplate(item) {
 
 renderCartContents();
 
-function renderCartContents() {
-  let cartItems = getLocalStorage("so-cart");
- 
-  if (cartItems && Array.isArray(cartItems)) {
- 
-    cartItems = cartItems.filter(item => item !== null);
- 
-    if (cartItems.length > 0) {
- 
-      document.querySelector(".cart-footer").classList.remove("hide");
- 
-      const total = cartItems.reduce((sum, item) => sum + (item.FinalPrice || 0), 0);
-      document.querySelector(".cart-total").innerText = `Total: $${total.toFixed(2)}`;
- 
-      const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-      document.querySelector(".product-list").innerHTML = htmlItems.join("");
-    } else {
-      showEmptyCart();
-    }
-  } else {
-    showEmptyCart();
-  }
-}
+
  
